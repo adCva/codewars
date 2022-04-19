01816e7360e917dd6231112b9a21277d903e84b7
adCva <adrcva@protonmail.com>
2022-04-19 21:22:05 +0300

    Extract the domain name from a URL

--- 6kyu/Convert string to camel case/case.php
+++ 6kyu/Convert string to camel case/case.php
@@ -2,7 +2,7 @@
 // Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
 
 function toCamelCase($str){
-    $words = preg_replace("/\_|-/", " ", $str;
+    $words = preg_replace("/\_|-/", " ", $str);
 
     if (preg_match("/[A-Z]/", $words[0])) {
         return str_replace(" ", "", ucwords($words));
